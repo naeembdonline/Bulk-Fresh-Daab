5955f5a2d5be7e34ecbe341948628cbbb739dab4
Z User <z@container>
2026-04-08 11:26:11 +0000

    8d9c5607-2238-4f21-bffa-9c589d80600d

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "standalone",
   typescript: {
     ignoreBuildErrors: true,
   },
@@ -9,6 +8,7 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true,
   },
+  allowedDevOrigins: ["*"],
 };
 
 export default nextConfig;
